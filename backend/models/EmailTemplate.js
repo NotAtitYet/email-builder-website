2e3341161c606f2f238e011abacaead3610032cd
Kumar Sonu <kumar.sonu.ece21@itbhu.ac.in>
2025-01-23 15:53:16 +0530

    changing schema for saving template

--- backend/models/EmailTemplate.js
+++ backend/models/EmailTemplate.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const emailTemplateSchema = new mongoose.Schema({
+  name: String,  // Template name
   title: String,
+  logo : String,
   content: String,
   footer: String,
-  imageUrls: [String],
+ // htmlContent: String Complete HTML content of the template
 });
 
 export default mongoose.model("EmailTemplate", emailTemplateSchema);
